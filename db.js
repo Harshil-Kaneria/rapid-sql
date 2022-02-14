4f094ae4b54d3e1ba55d1c8e9c6193ee510c65ee
Harshil Kaneria <36322779+Harshil-Kaneria@users.noreply.github.com>
2022-02-14 13:40:08 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 // get the client
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host:process.env.R_HOST,
-  user:process.env.R_USER,
-  password:process.env.R_PASSWORD,
-  database:process.env.R_DATABASE,
+  host:process.env.R_HOST || '',
+  user:process.env.R_USER || '',
+  password:process.env.R_PASSWORD || '',
+  database:process.env.R_DATABASE || '',
   waitForConnections: true,
   queueLimit: 0
 });
